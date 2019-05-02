67d1a9a39e977ab944b76e4831a27e82e4f81e5a
Andreas Karpasitis <karpasitis@outlook.com>
2019-05-02 14:28:13 +0200

    chore: add website links

--- src/components/Web.js
+++ src/components/Web.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import PortfolioImage from '../images/web/jpeg/portfolio.jpg';
+import EsolnetImage from '../images/web/jpeg/esolnet.jpg';
 import style from './Web.scss';
 
 const Home = () => {
@@ -9,9 +10,13 @@ const Home = () => {
       <hr />
       <p>Providing solutions for simple landing pages to websites for promoting your business, yourself and to show off your work.</p>
       <p>Setting up domains, emails, hosting space and annual maintenance.</p>
+      <div className={style.projects}>
+        <a href="https://www.karpasitis.net" target="_blank" rel="noopener noreferrer"><img src={PortfolioImage} alt="Portfolio Website" /></a>
+        <a href="https://www.esolnetespana.es" target="_blank" rel="noopener noreferrer"><img src={EsolnetImage} alt="Esolnet Espana" /></a>
+      </div>
       <p>Complex solutions to setup an online store or an online membership site. Projects can be discussed on a one to one basis to determine project feasibility and possible costs.</p>
       <p>Technologies that are used include but not limited to <strong>Wordpress</strong> or depending on the case, custom made using the web standard programming language, <strong>Javascript</strong> (with React framework) </p>
-      <p><strong>Contact us for further details and pricing.</strong></p>    
+      <p><strong>Contact us for further details and pricing.</strong></p>
     </div>
   );
 };
