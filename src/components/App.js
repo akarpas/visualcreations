01e9596c36316a8d62d8cb0c9338e7e76df1f7e0
Andreas Karpasitis <karpasitis@outlook.com>
2019-05-02 12:52:42 +0200

    chore: add basic burger menu button with toggle state

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
 
 import style from './App.scss';
 
-const App = () => (
-  <div className={style.wrapper}>
-    <header className={style.header}>
-      <h1 className={style.headerTitle}>Visual Creations</h1>
-    </header>
-    <p>React / Redux Boilerplate with Webpack. ESLint setup and SCSS Support</p>
-  </div>
-);
+const App = () => {
+  const [isBurgerOn, setIsBurgerOn] = useState(false);
+
+  const toggleMenu = () => {
+    setIsBurgerOn(!isBurgerOn);
+  }
+
+  return (
+    <div className={style.wrapper}>
+      <header className={style.header}>
+        <h1 className={style.headerTitle}>Visual Creations</h1>
+        <div className={style.menu}>
+          <button onClick={toggleMenu}>
+            {isBurgerOn
+              ? <i id="menu" className="material-icons">close</i>
+              : <i id="menu" className="material-icons">menu</i>}
+          </button>
+        </div>
+      </header>
+      <div className={style.content}>
+        <p>React / Redux Boilerplate with Webpack. ESLint setup and SCSS Support</p>
+      </div>
+    </div>
+  )
+};
 
 export default App;
