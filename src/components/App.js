8ce1de42f8c772c35f189456bbf18cb523a80883
Andreas Karpasitis <karpasitis@outlook.com>
2019-05-02 12:23:52 +0200

    chore: setup basic header

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
 
 import style from './App.scss';
 
 const App = () => (
   <div className={style.wrapper}>
-    <h1 className={style.header}>React Boilerplate</h1>
+    <header className={style.header}>
+      <h1 className={style.headerTitle}>Visual Creations</h1>
+    </header>
     <p>React / Redux Boilerplate with Webpack. ESLint setup and SCSS Support</p>
   </div>
 );
