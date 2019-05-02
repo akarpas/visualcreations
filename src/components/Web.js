0a08a2637f966709bdfb4ac70da2e7f33aebee85
Andreas Karpasitis <karpasitis@outlook.com>
2019-05-02 14:46:31 +0200

    chore: fix styling for images

--- src/components/Web.js
+++ src/components/Web.js
@@ -11,8 +11,12 @@ const Home = () => {
       <p>Providing solutions for simple landing pages to websites for promoting your business, yourself and to show off your work.</p>
       <p>Setting up domains, emails, hosting space and annual maintenance.</p>
       <div className={style.projects}>
-        <a href="https://www.karpasitis.net" target="_blank" rel="noopener noreferrer"><img src={PortfolioImage} alt="Portfolio Website" /></a>
-        <a href="https://www.esolnetespana.es" target="_blank" rel="noopener noreferrer"><img src={EsolnetImage} alt="Esolnet Espana" /></a>
+        <a href="https://www.karpasitis.net" target="_blank" rel="noopener noreferrer">
+          <img className={style.thumb} src={PortfolioImage} alt="Portfolio Website" />
+        </a>
+        <a href="https://www.esolnetespana.es" target="_blank" rel="noopener noreferrer">
+          <img className={style.thumb} src={EsolnetImage} alt="Esolnet Espana" />
+        </a>
       </div>
       <p>Complex solutions to setup an online store or an online membership site. Projects can be discussed on a one to one basis to determine project feasibility and possible costs.</p>
       <p>Technologies that are used include but not limited to <strong>Wordpress</strong> or depending on the case, custom made using the web standard programming language, <strong>Javascript</strong> (with React framework) </p>
