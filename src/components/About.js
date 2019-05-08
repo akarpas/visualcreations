9fe1e7eb103e9bbd80535d11349d4ad7f42b7cab
Andreas Karpasitis <karpasitis@outlook.com>
2019-05-08 11:43:04 +0200

    chore: update and correct text in about

--- src/components/About.js
+++ src/components/About.js
@@ -10,8 +10,8 @@ const About = () => {
       <hr />
       <Fade>
         <p>Based in Barcelona, Spain with a passion for creating visual and digital products, focusing on Photography, Video, Web and Software development.</p>
-        <p>Enjoying the worked produced and open to new challenges and collaborations to bring ideas to life and create new connections and produce results that satisfy the customers needs.</p>
-        <p>Offering freelance servies for short term contracts and jobs. Available for short term traveling, for specific types of work.</p>
+        <p>Enjoying the producing projects; open to new challenges and collaborations to bring ideas to life and creating new connections; producing results with the customers needs in mind.</p>
+        <p>Offering freelance services for short term contracts and jobs. Available for short term traveling, for specific types of work.</p>
       </Fade>
     </div>
   );
