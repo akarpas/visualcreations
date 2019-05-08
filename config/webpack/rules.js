911ad30a09453a052cef56535bf01e73934d3b0e
Andreas Karpasitis <karpasitis@outlook.com>
2019-05-08 16:08:59 +0200

    chore: update webpack rules to not rename favicon

--- config/webpack/rules.js
+++ config/webpack/rules.js
@@ -26,6 +26,14 @@ module.exports = [
     use: ['url-loader?limit=10000', 'img-loader'],
   },
   {
+    test: /\.(ico)$/i,
+    use: ['url-loader?limit=10000', 'img-loader', 'file-loader?name=[name].[ext]'],
+  },
+  // {
+  //   test: /\.jpe?g$|\.ico$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+  //   loader: 'file-loader?name=[name].[ext]'  // <-- retain original file name
+  // },
+  {
     test: /\.s(a|c)ss$/,
     use: [
       {
