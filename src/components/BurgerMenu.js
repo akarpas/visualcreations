3913a1ba611a142fd0b1c765836bc6b7034cd418
Andreas Karpasitis <karpasitis@outlook.com>
2019-05-11 18:15:18 +0200

    chore: implement landscape phone media queries

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -23,12 +23,16 @@ const BurgerMenu = props => {
               <i id="menu" className="material-icons">close</i>
             </button>
           </div>
-          <Link onClick={toggleMenu} to="/">Home</Link>
-          <Link onClick={toggleMenu} to="/web">Websites</Link>
-          <Link onClick={toggleMenu} to="/software">Software</Link>
-          <Link onClick={toggleMenu} to="/photography">Photo/Video</Link>
-          <Link onClick={toggleMenu} to="/about">About</Link>
-          <Link onClick={toggleMenu} to="/contact">Contact</Link>
+          <div className={style.sectionOne}>
+            <Link onClick={toggleMenu} to="/">Home</Link>
+            <Link onClick={toggleMenu} to="/web">Websites</Link>
+            <Link onClick={toggleMenu} to="/software">Software</Link>
+          </div>
+          <div className={style.sectionTwo}>
+            <Link onClick={toggleMenu} to="/photography">Photo & Video</Link>
+            <Link onClick={toggleMenu} to="/about">About</Link>
+            <Link onClick={toggleMenu} to="/contact">Contact</Link>
+          </div>
         </div>
       </div>
     </Flip>
